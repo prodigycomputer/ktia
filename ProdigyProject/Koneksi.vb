08d810f0e41be092c7e09b5c24f12066b3f4c660
prodigycomputer <setiabudi21a@gmail.com>
2025-10-02 15:54:10 +0700

    Add files via upload

---
+++ ProdigyProject/Koneksi.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Data.Odbc
+Module Koneksi
+    Public Conn As OdbcConnection
+    Public Cmd As OdbcCommand
+    Public Da As OdbcDataAdapter
+    Public Ds As DataSet
+    Public Rd As OdbcDataReader
+    Public Trans As OdbcTransaction
+
+    Public Sub BukaKoneksi()
+        Try
+            ' --- koneksi langsung tanpa DSN ---
+            Dim connStr As String = "Driver={MySQL ODBC 5.1 Driver};Server=192.168.1.123;Database=dbkita;User=kita;Password=kita;Option=3;"
+            Conn = New OdbcConnection(connStr)
+
+            If Conn.State = ConnectionState.Closed Then
+                Conn.Open()
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Gagal koneksi: " & ex.Message)
+        End Try
+    End Sub
+End Module
